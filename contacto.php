02fbde271aeafbbdb3dcf2a0086d9a5efaf3c793
Franklin Belen <franklin_al_love@hotmail.com>
2018-08-10 10:51:47 -0500

    Version 1.0.0

---
+++ contacto.php
@@ -0,0 +1,20 @@
+<section class="section" id="contacto">
+    <h2>ESTEMOS EN CONTACTO</h2>
+    <p>POR FAVOR, LLENE EL FORMULARIO DE FUEGO PARA PONERSE EN CONTACTO.</p>
+            
+    <form action="">
+        <label for="">NOMBRE</label>
+        <input type="text" placeholder="Tu Nombre">
+                
+        <label for="">EMAIL</label>
+        <input type="text" placeholder="Tu E-mail">
+                
+        <label for="">TEMA</label>
+        <input type="text" placeholder="Tu Tema">
+                
+        <label for="">MENSAJE</label>
+        <input type="text" placeholder="Tu Mensaje">
+
+        <input type="submit" value="ENVIAR">
+    </form>
+</section>
